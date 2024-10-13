e8f2725e1987459155949ecd2f3529a049a90f46
“Ashadulla” <ashadullaalgalib@gmail.com>
2024-10-13 12:41:08 +0600

    Added navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
-<template>
-  <div>
-    <h1>Hello Word!</h1>
-  </div>
-</template>
-
-<script>
-export default {}
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue'
 </script>
-
-<style lang="scss" scoped></style>
+<template>
+  <Navbar />
+  <Footer />
+</template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="A">
+    This is footer
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped></style>
