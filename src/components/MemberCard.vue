ad360b5a8c948086f7d3becd2d172f4c918a59d7
Ashek Elahi <ashek.elahi596@gmail.com>
2024-10-28 14:35:22 +0600

    ADDED: About page new design added

---
+++ src/components/MemberCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <RouterLink to="/about" class="member-card">
+    <div class="image">
+      <img alt="main logo" src="@/assets/images/dummy-member.jpg" />
+    </div>
+    <div class="caption">
+      <h5 class="name">John Doe</h5>
+      <p class="designation">Chairman</p>
+    </div>
+  </RouterLink>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.member-card {
+  flex-basis: calc((100% - 90px) / 4);
+  background-color: aquamarine;
+  position: relative;
+  overflow: hidden;
+
+  &:hover {
+    .image {
+      transform: scale(1.1);
+    }
+  }
+
+  .image {
+    width: 100%;
+    transition: all 0.5s;
+
+    img {
+      display: block;
+      width: 100%;
+    }
+  }
+
+  .caption {
+    background: rgba(16, 89, 122, 0.8);
+    color: var(--vt-c-white);
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    padding: 10px;
+    text-align: center;
+
+    .name {
+      color: inherit;
+      font-size: 18px;
+      font-weight: 500;
+    }
+
+    .designation {
+      color: inherit;
+      font-size: 14px;
+      font-weight: 300;
+    }
+  }
+}
+</style>
--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -5,8 +5,12 @@
       Click on the x button in the top right corner to close the current tab:
     </p>
     <div class="tab">
-      <button class="tablinks" v-for="tabItem in tabItems" @click="activeTab = tabItem"
-        :class="[activeTab == tabItem ? 'active' : '']">
+      <button
+        class="tablinks"
+        v-for="tabItem in tabItems"
+        @click="activeTab = tabItem"
+        :class="[activeTab == tabItem ? 'active' : '']"
+      >
         {{ tabItem }}
       </button>
     </div>
@@ -21,16 +25,30 @@
     </div>
 
     <div class="tabcontent" v-if="activeTab == tabItems[1]">
-      <h3>Tab-02 Content</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
-        dicta nobis placeat odit tempore neque quibusdam asperiores earum culpa
-        mollitia excepturi atque, optio dolorum iste velit sit minima nemo? Hic.
-      </p>
+      <div class="members">
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+      </div>
     </div>
 
     <div class="tabcontent" v-if="activeTab == tabItems[2]">
-      <h3>Tab-03 Content</h3>
+      <div class="members">
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+      </div>
+    </div>
+    <div class="tabcontent" v-if="activeTab == tabItems[3]">
+      <h3>Vision & Values</h3>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
         dicta nobis placeat odit tempore neque quibusdam asperiores earum culpa
@@ -42,14 +60,24 @@
 
 <script setup>
 import { ref } from 'vue'
+import MemberCard from '@/components/MemberCard.vue'
 const isMenuOpen = ref(false)
-let tabItems = ['Tab1', 'Tab2', 'Tab3']
+let tabItems = [
+  'Our Profile',
+  'Board of Directors',
+  'Management Tam',
+  'Vision & Values',
+  'Recognition',
+  'Publications',
+  'Sustainability Report',
+  'CSR',
+]
 const isActiveLink = routePath => {
   const route = useRoute()
   return route.path === routePath
 }
 
-const activeTab = ref('Tab1')
+const activeTab = ref(tabItems[0])
 </script>
 
 <style lang="scss" scoped>
@@ -115,8 +143,69 @@ const activeTab = ref('Tab1')
 
   .tabcontent {
     position: relative;
-    padding: 6px 12px;
+    padding: 30px 12px;
     animation: tabContentAnimation 1s ease-in-out forwards;
+
+    .members {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 30px;
+    }
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .tabs {
+    width: 90%;
+
+    .tab {
+      gap: 5px;
+      padding-bottom: 10px;
+      justify-content: flex-start;
+      width: 100%;
+      overflow-x: scroll;
+
+      &::after {
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 3px;
+        border-radius: 100px;
+        background-color: #cccccc83;
+      }
+
+      button {
+        padding: 5px 15px;
+        font-size: 14px;
+        border-radius: 25px;
+
+        &:hover {
+          background-color: #ddd;
+        }
+
+        &.active {
+          background-color: var(--color-primary);
+          color: #fff;
+
+          &::after {
+            content: '';
+            position: absolute;
+            bottom: -10px;
+            left: 0;
+            width: 100%;
+            height: 6px;
+            border-radius: 100px;
+            background-color: var(--color-primary);
+            z-index: 1;
+          }
+        }
+      }
+    }
+
+    .tabcontent {
+      position: relative;
+      padding: 6px 12px;
+    }
   }
 }
 
