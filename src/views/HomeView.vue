699001a8d968372a00461bb83f4c188321fe297f
“Ashadulla” <ashadullaalgalib@gmail.com>
2024-12-26 14:17:43 +0600

    Added sustainability component and updated some changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,14 @@ import HomeSlider from '@/components/HomeSlider.vue';
 import About from '@/components/AboutUs/About.vue';
 import OurProducts from '@/components/Products/OurProducts.vue';
 import Achievement from '@/components/AboutUs/Achievement.vue';
+import Sustainability from '@/components/AboutUs/Sustainability.vue';
 </script>
 
 <template>
   <HomeSlider />
   <About />
   <Achievement />
+  <Sustainability />
   <OurProducts />
 </template>
 
